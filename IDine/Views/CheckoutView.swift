118be61d2132067ae9f483154afd0a3b3c86c8bd
Giannini Charles <12gcharles@gmail.com>
2020-07-04 16:42:44 -0400

    updated checkout

--- IDine/Views/CheckoutView.swift
+++ IDine/Views/CheckoutView.swift
@@ -15,9 +15,16 @@ struct CheckoutView: View {
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 1
+    @State private var showingPaymentAlert = false
 
     static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     static let tipAmounts = [10, 15, 20, 25, 0]
+
+    var totalPrice: Double {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(Self.tipAmounts[tipAmount])
+        return total + tipValue
+    }
 
     var body: some View {
         Form {
@@ -43,10 +50,16 @@ struct CheckoutView: View {
                 }.pickerStyle(SegmentedPickerStyle())
             }
 
-            Section(header: Text("TOTAL: $100")) {
-                Button("Confirm order") {
-                    print("SENDING ORDER TO SERVER")
-                }
+            Section(header:
+                Text("TOTAL: $\(totalPrice, specifier: "%.2f")")
+                    .font(.largeTitle)) {
+                        Button("Confirm order") {
+                            self.showingPaymentAlert.toggle()
+                        }
+            }
+
+            .alert(isPresented: $showingPaymentAlert) { () -> Alert in
+                Alert(title: Text("Order confirmed"), message: Text("Your total was $\(totalPrice, specifier: "%.2f") - thank you!" ), dismissButton: .default(Text("OK")))
             }
         }
         .navigationBarTitle(Text("Payment"), displayMode: .inline)
--- IDine/Views/OrderView.swift
+++ IDine/Views/OrderView.swift
@@ -21,20 +21,27 @@ struct OrderView: View {
                             Spacer()
                             Text("$\(item.price)")
                         }
-                    }
+                    }.onDelete(perform: deleteItems)
                 }
 
                 Section {
                     NavigationLink(destination: CheckoutView()) {
                         Text("Place Order")
                     }
-                }
+                }.disabled(order.items.isEmpty)
             }
         .navigationBarTitle("Order")
         .listStyle(GroupedListStyle())
+            .navigationBarItems(trailing: EditButton())
         }
 
     }
+
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
+    }
+
+
 }
 
 struct OrderView_Previews: PreviewProvider {
