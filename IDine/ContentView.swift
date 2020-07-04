9badabc6a5fa9563872d00b71d2f1a313cf9aca3
Giannini Charles <12gcharles@gmail.com>
2020-07-04 12:04:03 -0400

    initial commit

---
+++ IDine/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  IDine
+//
+//  Created by Giannini Charles on 7/4/20.
+//  Copyright © 2020 GCharlesStudy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(menu) { section in
+                    Section(header:  Text(section.name)) {
+                        ForEach(section.items) { item in
+                                           Text(item.name)
+                                       }
+                    }
+                }
+            }
+            .navigationBarTitle("Menu")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
